06da2f3a7eb22d852d6dd2e5710ff2daf2ce9933
ZavodszkiM <99268948+ZavodszkiM@users.noreply.github.com>
2022-04-13 19:20:49 +0200

    10. házi feladat

---
+++ B2C7B0_0413/msgctl.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdlib.h>
+#define MSGKEY 654321L
+
+int main()
+{
+	int msgid, msgflg,  rtn;
+	key_t kulcs;
+	kulcs = MSGKEY;
+	msgflg = 00666 | IPC_CREAT;
+	msgid = msgget(kulcs, msgflg);
+
+	rtn = msgctl(msgid, IPC_RMID, NULL);
+	printf ("\n Vissztért: %d", rtn);
+
+	exit (0);
+}
